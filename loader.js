fbe086659fc0ab28e85695864be21f4b40d277c7
FancyBoiL1 <117115118+FancyBoiL1@users.noreply.github.com>
2024-02-23 08:44:44 -0800

    Update and rename base.js to loader.js

--- loader.js
+++ loader.js
@@ -1,9 +1,16 @@
 function loadduri(link){
   var iframe = document.createElement('iframe');   
   iframe.sandbox.add('allow-top-navigation');
+  iframe.sandbox.add('allow-same-origin');
   iframe.sandbox.add('allow-scripts');
-  iframe.sandbox.add('allow-popups-to-escape-sandbox');
   iframe.sandbox.add('allow-popups');
+  iframe.sandbox.add('allow-popups-to-escape-sandbox');
+  iframe.sandbox.add('allow-forms');
+  iframe.sandbox.add('allow-modals');
+  iframe.sandbox.add('allow-orientation-lock');
+  iframe.sandbox.add('allow-pointer-lock');
+  iframe.sandbox.add('allow-presentation');
+  iframe.sandbox.add('allow-pointer-lock');
   iframe.src = link;
   iframe.allowfullscreen = 'true';
   iframe.allowtransparency='true';
